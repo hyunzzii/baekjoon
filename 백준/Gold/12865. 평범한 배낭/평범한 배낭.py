f4137ef5ed076ac7c339f52e3388156eb3ba218f
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2023-07-05 18:07:44 +0900

    [Gold V] Title: 평범한 배낭, Time: 328 ms, Memory: 194320 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,9 +1,7 @@
 n,k = map(int,input().split())
-arr = [[0 for i in range(k+10)] for j in range(n+10)]
-
+arr = [[0 for i in range(k+1)] for j in range(n+1)]
 for i in range(1,n+1) :
   w,v = map(int,input().split())
-  arr[i][w] = v
   for j in range(1,k+1) :
     if j < w :
       arr[i][j] = arr[i-1][j]
