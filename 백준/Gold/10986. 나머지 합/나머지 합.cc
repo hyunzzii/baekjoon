6ce8a1a1c236d933c2ac30b314e51b4b934dc9ca
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2024-03-14 14:54:18 +0900

    [Gold III] Title: 나머지 합, Time: 136 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.cc
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+#include <vector>
 using namespace std;
 
 int main(void){
@@ -11,19 +12,19 @@ int main(void){
     long long int tem,sum=0;
     cin >> N >> M;
     
-    int* arr = new int[N+1];
-    long long int* mod = new long long int[M];
+    vector<int> arr;
+    vector<long long int> mod;
 
-    fill_n(mod,M,0);
-    arr[0] = 0;
-
+    mod.assign(M,0);
+    arr.assign(N+1,0);
+    arr[0]=0;
+    
     for(int i=1; i<N+1; i++){
         cin >> tem;
-        tem = (arr[i-1] + (tem % M)) % M;
+        tem = (arr[i-1] + tem) % M;
         arr[i] = tem;
         sum += mod[tem];
         mod[tem]++;
     }
     cout << sum + mod[0];
-    delete[] arr;
 }
