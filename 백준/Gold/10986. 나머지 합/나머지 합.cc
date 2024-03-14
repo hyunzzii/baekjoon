5bb9337259a00a37cac04fdc40342d9d27f6b069
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2024-03-14 14:39:23 +0900

    [Gold III] Title: 나머지 합, Time: 152 ms, Memory: 9836 KB -BaekjoonHub

---
+++ 백준/Gold/10986. 나머지 합/나머지 합.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    long long int N,M;
+    long long int tem,sum=0;
+    cin >> N >> M;
+    
+    long long int* arr = new long long int[N+1];
+    long long int* mod = new long long int[M];
+
+    fill_n(mod,M,0);
+    arr[0] = 0;
+
+    for(int i=1; i<N+1; i++){
+        cin >> tem;
+        tem = (arr[i-1] + (tem % M)) % M;
+        arr[i] = tem;
+        sum += mod[tem];
+        mod[tem]++;
+    }
+    cout << sum + mod[0];
+    delete[] arr;
+}
