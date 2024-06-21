4d86fb15b6cc09cf4c7f629d05572ef30815886e
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2024-06-21 19:22:52 +0900

    [Silver II] Title: 스택 수열, Time: 68 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+int main(void){
+    int n, now, num=1;
+    stack<int> s;
+    cin >> n;
+    
+    vector<string> result;
+    while(n--){
+        cin >> now;
+        while(true){
+            if(s.size() && s.top() == now){
+                s.pop();
+                result.push_back("-");
+                break;
+            }
+            if(s.size() && s.top() > now){
+                cout << "NO";
+                return 0;
+            }
+            s.push(num++);
+            result.push_back("+");
+        }
+    }
+    for(int i=0;i<result.size();i++){
+        cout << result[i] << "\n";
+    }
+}
