aa0aeafe6973b818164329c6b0f12e7bffd30ecd
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2024-06-25 16:39:42 +0900

    [Gold IV] Title: 오큰수, Time: 208 ms, Memory: 13620 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,26 +1,24 @@
 #include <iostream>
-#include <stack>
 
 using namespace std;
 typedef pair<int,int> node;
 
-int result[1000001];
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n, tem;
-    stack<node> stack;
+    int n, tem, top=0, result[1000001]={0,};
     
     cin >> n;
+    node* stack = new node[n];
+
     for(int i=0;i<n;i++){
         cin >> tem;
-        while(!stack.empty() && stack.top().first < tem){
-            result[stack.top().second] = tem;
-            stack.pop();
+        while(top && stack[top].first < tem){
+            result[stack[top--].second] = tem;
         }
-        stack.push(make_pair(tem,i));
+        stack[++top] = make_pair(tem,i);
     }
     for(int i=0;i<n;i++){
         cout << (result[i] ? result[i] : -1) << ' ';
