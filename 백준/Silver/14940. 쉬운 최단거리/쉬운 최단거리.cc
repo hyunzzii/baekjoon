07a214cf594f25ba1d6c85a425038db818b33149
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2024-07-31 01:36:49 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 40 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.cc
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.cc
@@ -8,7 +8,25 @@ int dy[4] = {1,-1,0,0};
 int n, m, arr[1000][1000], ans[1000][1000];
 queue<node> que;
 
-void bfs(){
+int main(int argc, char const *argv[]){
+	ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+	cin >> n >> m;
+	
+	for(int x=0; x<n; x++){
+		for(int y=0; y<m; y++){
+			cin >> arr[x][y];
+			ans[x][y] = arr[x][y] ? -1 : 0;
+			if(arr[x][y] == 2){
+				que.push(make_pair(x,y));
+				ans[x][y]=0;
+				arr[x][y]=3;
+			}
+		}
+	}
+	
 	while(!que.empty()){
 		node s = que.front();
 		int x = s.first;
@@ -28,27 +46,6 @@ void bfs(){
 			}
 		}
 	}
-}
-
-int main(int argc, char const *argv[]){
-	ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-
-	cin >> n >> m;
-	
-	for(int x=0; x<n; x++){
-		for(int y=0; y<m; y++){
-			cin >> arr[x][y];
-			ans[x][y] = arr[x][y] ? -1 : 0;
-			if(arr[x][y] == 2){
-				que.push(make_pair(x,y));
-				ans[x][y]=0;
-				arr[x][y]=3;
-			}
-		}
-	}
-	bfs();
 
 	for(int x=0; x<n; x++){
 		for(int y=0; y<m; y++){
