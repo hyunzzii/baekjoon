a94f3111c534cf751ebc4bf7d81daf0d338b2e54
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2024-08-06 22:27:26 +0900

    [Silver III] Title: 파도반 수열, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.cc
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -2,18 +2,10 @@
 using namespace std;
 
 int main(void){
-    long long int arr[5] = {2,1,1,1,2};
-    long long int dp[101];
+    long long int dp[101] = {0,1,1,1,2,2,3};
 
-    dp[1]=1;
-    dp[2]=1;
-    dp[3]=1;
-    dp[4]=2;
-
-    for(int i=5,num=0; i<101; i++){
-        dp[i] = arr[num];
-        arr[(num+1)%5] += arr[num];
-        num = (num+1)%5;
+    for(int i=7,num=0; i<101; i++){
+        dp[i] = dp[i-1]+dp[i-5];
     }
 
     int T,n;
