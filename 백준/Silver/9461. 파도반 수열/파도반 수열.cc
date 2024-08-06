765dec84789b7b3b276039ba85b180e5d1144111
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2024-08-06 22:20:56 +0900

    [Silver III] Title: 파도반 수열, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.cc
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -1,23 +1,8 @@
 #include <stdio.h>
-#include <utility>
 using namespace std;
 
-long long int arr[5] = {2,1,1,1,2};       //반시계방향
-
-long long int max(){
-    long long int max = 0;
-    int index;
-    for(int i=0;i<5;i++){
-        if(max<arr[i]){
-            max=arr[i];
-            index=i;
-        }
-    }
-    arr[(index+1)%5] += arr[index];
-    return max;
-}
-
 int main(void){
+    long long int arr[5] = {2,1,1,1,2};
     long long int dp[101];
 
     dp[1]=1;
@@ -25,8 +10,10 @@ int main(void){
     dp[3]=1;
     dp[4]=2;
 
-    for(int i=5;i<101;i++){
-        dp[i] = max();
+    for(int i=5,num=0; i<101; i++){
+        dp[i] = arr[num];
+        arr[(num+1)%5] += arr[num];
+        num = (num+1)%5;
     }
 
     int T,n;
