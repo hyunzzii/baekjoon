49b109514f82dac628b054e028cc2b1f607af79d
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2024-08-14 09:36:08 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -1,33 +1,19 @@
-// #include <stdio.h>
-#include <iostream>
-
+#include <stdio.h>
 using namespace std;
 
 int main(void){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
-    int n;
-    cin >> n;
-
-    int arr[n],tem;
-    for(int i=0;i<n;i++){
-        cin >> arr[i];
-    }
-    for(int i=0;i<n;i++){
-        int flag = 1;
-        for(int j=1;j<n-i;j++){
-            if(arr[j-1]>arr[j]){
-                tem = arr[j-1];
-                arr[j-1] = arr[j];
-                arr[j] = tem;
-                flag = 0;
-            }
-        }
-        if(flag) break;
-    }
+    int N,tem,num=0;
+    int arr[2001] = {0,};
     
-    for(int i=0;i<n;i++){
-        cout << arr[i] <<'\n';
+    scanf("%d",&N);
+    while(N--){
+        scanf("%d",&tem);
+        arr[1000 + tem] = 1;
+    }
+    for(int i = 0; i < 2001; i++){
+        if(arr[i]){
+            printf("%d\n",i-1000);
+            if(++num == N) return 0;
+        }
     }
 }
