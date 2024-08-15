8756b3d5b65e17eef0b369647e019256b7e12afe
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2024-08-15 22:51:10 +0900

    [Silver II] Title: 에디터, Time: 60 ms, Memory: 20768 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int main(void){
+    ios_base :: sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    list<char> lst;
+    char c;
+
+    while((c=cin.get())!='\n'){
+        lst.push_back(c);
+    }
+
+    list<char>::iterator cursor = lst.end(),tem;
+    string str;
+    int t;
+
+    cin >> t;
+    cin.ignore();
+    while(t--){
+        getline(cin,str);
+        if(str.at(0)=='L'){
+            if(cursor != lst.begin()) cursor--;
+            continue;
+        }
+        if(str.at(0)=='D'){
+            if(cursor != lst.end()) cursor++;
+            continue;
+        }
+        if(str.at(0)=='B'){
+            if(cursor == lst.begin()) continue;
+            if(cursor == lst.end()){
+                lst.erase(--cursor);
+                cursor = lst.end();
+                continue;
+            }
+            cursor = lst.erase(--cursor);
+            continue;
+        }
+        if(str.at(0)=='P'){
+            lst.insert(cursor,str[2]);
+        }
+    }
+    for(char c : lst){
+        cout << c;
+    }
+}
