8c00688aa8e8cee381d246d2daba956a18a21ac0
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2024-08-16 20:36:45 +0900

    [Silver V] Title: K번째 수, Time: 648 ms, Memory: 51300 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.cc
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -1,22 +1,64 @@
 #include <iostream>
-#include <queue>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+vector<int> v;
+int k,n;
+void swap(int num1,int num2){
+    int tem = v[num1];
+    v[num1] = v[num2];
+    v[num2] = tem;
+}
+
+int quickSort(int start, int end){
+    int pivot = (start+end)/2;
+    swap(start,pivot);
+    pivot = start++;
+
+    while(start<end){
+        if(v[start] < v[pivot]){
+            start++;
+        }
+        else if(v[end] > v[pivot]){
+            end--;
+        }
+        else{
+            swap(start,end);
+            start++;end--;
+        }
+    }
+    if(v[pivot] > v[start]){
+        swap(start,pivot);
+        return start;
+    }
+    else{
+        swap(--start,pivot);
+        return start;
+    }
+}
+
 int main(void){
-    ios_base :: sync_with_stdio(false);
+    ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n,k,tem;
+    int tem;
     cin >> n >> k;
 
-    priority_queue<int> pq;
     for(int i=0;i<n;i++){
         cin >> tem;
-        pq.push(tem);
+        v.emplace_back(tem);
     }
-    while(n-->k){
-        pq.pop();
-    }
-    cout << pq.top();
-}
+    int s=0, e=n-1,r=k--;
+    do{
+        r = quickSort(s,e);
+        if(k<r){
+            e = r-1;
+        }
+        else{
+            s = r+1;
+        }
+    }while(r!=k && s!=e);
+    cout << v[k];
+} 
