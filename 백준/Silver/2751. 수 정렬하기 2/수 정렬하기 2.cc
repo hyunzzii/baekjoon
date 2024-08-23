79003b63b84d897229eb84dac160fcfa2d339a18
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2024-08-23 15:54:25 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 200 ms, Memory: 9712 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -1,55 +1,29 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-
-vector<int> v;
-vector<int> tem;
-
-void merge_sort(int s, int e){
-    int m = (e+s)/2;
-    int s2 = m+1;
-
-    if(s<m){
-        merge_sort(s,m);
-        merge_sort(m+1,e);
-        for(int i=s;i<=e;i++){
-            tem[i] = v[i];
-        }
-    }
-    int i=s;
-    while(s<=m && s2<=e){
-        if(tem[s] < tem[s2]){
-            v[i++] = tem[s++];
-        }
-        else{
-            v[i++] = tem[s2++];
-        }
-    }
-    while(s<=m){
-        v[i++] = tem[s++];
-    }
-     while(s2<=e){
-        v[i++] = tem[s2++];
-    }
-}
+#define min(a,b) (a<b)?a:b;
+#define max(a,b) (a>b)?a:b;
 
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     
-    int n;
+    int n,tem;
     cin >> n;
 
-    v = vector<int>(n,0);
-    tem = vector<int>(n,0);
+    int arr[2000001]={0,},base=1000000;
+    int min = base, max = -base;
 
     for(int i=0;i<n;i++){
-        cin >> tem[i];
+        cin >> tem;
+        arr[base+tem] = 1;
+        min = min(tem,min);
+        max = max(tem,max);
     }
-    merge_sort(0,n-1);
 
-    for(int i=0;i<n;i++){
-        cout << v[i] << '\n';
+    for(int i=min;i<=max;i++){
+        if(arr[i+base]){
+            cout << i << '\n';
+        }
     }
 } 
