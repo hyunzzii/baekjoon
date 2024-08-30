f8261d56a932b6b4746332c5da5d7b14ebe314e1
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2024-08-30 09:39:30 +0900

    [Silver IV] Title: 주몽, Time: 0 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -2,28 +2,28 @@
 #include <vector>
 using namespace std;
 
-
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
     long long int N,M;
-    int sum=0;
+    int sum=0,tem;
     int num[100001] = {0,};
+    
     cin >> N >> M;
     if(M/2 > 100000){
         cout << sum;
          return 0;
     }
+    
     while(N--){
-        int tem;
         cin >> tem;
         num[tem] = 1;
     }
-    for(int i=1;i<=M/2;i++){
+    for(int i=1;i<(double)M/2;i++){
         if(num[i]!=1) continue;
-        if(M-i!=i && num[M-i]==1){
+        if(num[M-i]==1){
             sum++;
         }
     }
