8234caef9b03f7f6d56d5f1344b3003bc6bf4463
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2024-09-10 19:05:40 +0900

    [Platinum V] Title: 버블 소트, Time: 104 ms, Memory: 5932 KB -BaekjoonHub

--- 백준/Platinum/1517. 버블 소트/버블 소트.cc
+++ 백준/Platinum/1517. 버블 소트/버블 소트.cc
@@ -8,22 +8,18 @@ vector<int> tem;
 long long int result = 0;
 
 void merge_sort(int s, int e){
+    if(e - s < 1)return;
+    
     int m = (e+s)/2;
     int s2 = m+1;
 
-    if(s<m){
-        merge_sort(s,m);
-        merge_sort(s2,e);
-        for(int i=s;i<=e;i++){
-            tem[i] = v[i];
-        }
+    merge_sort(s,m);
+    merge_sort(s2,e);
+    for(int i=s;i<=e;i++){
+        tem[i] = v[i];
     }
 
     int i=s;
-    if(s>m || s2> e){
-        v[s] = tem[s];
-        return;
-    }
     while(s<=m && s2<=e){
         if(tem[s] <= tem[s2]){
             v[i++] = tem[s++];
@@ -53,7 +49,7 @@ int main(void){
     tem = vector<int>(n,0);
 
     for(int i=0;i<n;i++){
-        cin >> tem[i];
+        cin >> v[i];
     }
     merge_sort(0,n-1);
     cout << result;
