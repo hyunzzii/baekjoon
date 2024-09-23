92e70bb535a0ca60b0e0b5bfaa476754fda25c00
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2024-09-23 21:23:12 +0900

    [Gold V] Title: 신기한 소수, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+vector<vector<int>> pn;
+
+bool isPrime(int num){
+    int root = sqrt(num);
+    for(int i=2;i<=root;i++){
+        if(num%i==0){
+            return false;
+        }
+    }
+    return true;
+} 
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n;
+    cin >> n;
+    pn.resize(n+1);
+
+    pn[1].push_back(2);
+    pn[1].push_back(3);
+    pn[1].push_back(5);
+    pn[1].push_back(7);
+
+    int x[4]={1,3,7,9};
+
+    for(int i=1;i<n;i++){
+        for(int n : pn[i]){
+            for(int j=0;j<4;j++){
+                int tem = n*10+x[j];
+                if(isPrime(tem)){
+                    pn[i+1].push_back(tem);
+                }
+            }
+        }
+    }
+
+    for(int n : pn[n]){
+        cout << n <<"\n";
+    }
+}
