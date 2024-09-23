94390dfebd35ed55aa2e4ba7a819fa168e072dc2
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2024-09-23 20:54:32 +0900

    Update 소수 찾기.c
    주석 추가

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.c
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.c
@@ -1,7 +1,21 @@
 #include<stdio.h>
 #include <math.h>
-void math(int s,int N,int*result)
-{for (; s > 1; s--) {if (N % s == 0 || s == 1)break;}
-	if(s==1&&N!=1)	*result += 1;}
-int main() {int T,N,sum,result=0;scanf("%d", &T);
-	for (; T > 0; T--){scanf("%d", &N);math(sqrt(N),N,&result);}printf("%d", result);}
+
+int result = 0;
+
+void math(int s,int N){
+    for (; s > 1; s--) {
+        if (N % s == 0)break;
+    }
+	if(s==1 && N!=1)result++;
+}
+// 루트값 보다 작은 애들이 인수가 될테니...
+int main() {
+    int T,N,sum;
+    scanf("%d", &T);
+	for (; T > 0; T--){
+        scanf("%d", &N);
+        math(sqrt(N),N);
+    }
+    printf("%d", result);
+}
