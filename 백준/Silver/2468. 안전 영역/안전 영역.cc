1f85c16869588d16f90cf1d3f8ba0de11186ccc9
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2024-09-24 22:03:49 +0900

    [Silver I] Title: 안전 영역, Time: 20 ms, Memory: 1540 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <cstdio>
 using namespace std;
     
 int arr[100][100], n, h;
@@ -11,44 +11,35 @@ void search(int x, int y, int visited[100][100]){
         x1 = x+dx[i];
         y1 = y+dy[i];
         if(x1 < 0 || y1 < 0 || x1 >= n || y1 >= n) continue;
-        if(arr[x1][y1]>0 && !visited[x1][y1]){
+        if(arr[x1][y1]>h && !visited[x1][y1]){
             visited[x1][y1] = 1;
             search(x1,y1,visited);
         }
     }
 }
 int main(void){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    cin >> n;
+    scanf("%d",&n);
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            cin >> arr[i][j];
+            scanf("%d",&arr[i][j]);
         }
     }
     int max=1;
-    while(true){
-        int visited[100][100]={0,};
-        int sum=0,flag=0;
 
+    for(h=0;h<100;h++){
+        int visited[100][100]={0,};
+        int sum=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                arr[i][j]--;
-            }
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(arr[i][j]>0 && !visited[i][j]){
+                if(arr[i][j]>h && !visited[i][j]){
                     visited[i][j] = 1;
                     search(i,j,visited);
                     sum++;
                 }
             }
         }
-        if(!sum)break;
+        if(!sum) break;
         if(max < sum) max = sum;
     }
-    cout << max;
+    printf("%d",max);
 } 
