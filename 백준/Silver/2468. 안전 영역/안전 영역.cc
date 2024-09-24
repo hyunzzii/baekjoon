3c2f12117760b9b33d79945b04675bb947402113
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2024-09-24 22:22:21 +0900

    [Silver I] Title: 안전 영역, Time: 12 ms, Memory: 1380 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -1,61 +1,47 @@
-#include <iostream>
-#include <queue>
+#include <cstdio>
 using namespace std;
     
-int arr[100][100], v[100][100], n;
+int arr[100][100], v[100][100],n, h;
 int dx[4] = {1,-1,0,0};
 int dy[4] = {0,0,1,-1};
-queue<pair<int,int>> que;
 
-void BFS(int x, int y){
-    v[x][y] = 0;
-    que.push(make_pair(x,y));
+void search(int x, int y){
     int x1,y1;
-    while(!que.empty()){
-        x = que.front().first;
-        y = que.front().second;
-        que.pop();
-
-        for(int i=0;i<4;i++){
-            x1 = x+dx[i];
-            y1 = y+dy[i];
-            if(x1 < 0 || y1 < 0 || x1 >= n || y1 >= n) continue;
-            if(v[x1][y1]){
-                v[x1][y1] = 0;
-                que.push(make_pair(x1,y1));
-            }
+    for(int i=0;i<4;i++){
+        x1 = x+dx[i];
+        y1 = y+dy[i];
+        if(x1 < 0 || y1 < 0 || x1 >= n || y1 >= n) continue;
+        if(v[x1][y1]){
+            v[x1][y1] = 0;
+            search(x1,y1);
         }
     }
 }
 int main(void){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    cin >> n;
+    scanf("%d",&n);
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            cin >> arr[i][j];
+            scanf("%d",&arr[i][j]);
         }
     }
-    int max=1;
+    int max=1,sum;
 
-    for(int h=0;h<100;h++){
+    for(h=0;h<100;h++){
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(arr[i][j]>h){
                     v[i][j]=1;
-                }
-                else{
+                }else{
                     v[i][j]=0;
                 }
             }
         }
-        int sum=0;
+        sum=0;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(v[i][j]){
-                    BFS(i,j);
+                    v[i][j]=0;
+                    search(i,j);
                     sum++;
                 }
             }
@@ -63,5 +49,5 @@ int main(void){
         if(!sum) break;
         if(max < sum) max = sum;
     }
-    cout << max;
+    printf("%d",max);
 } 
