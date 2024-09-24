267cdc2e68e784bc4b96cf3af002eb253a535601
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2024-09-24 15:36:30 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 40 ms, Memory: 8060 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -4,18 +4,15 @@
 using namespace std;
 
 vector<vector<int>> graph;
-vector<int> visited;
 vector<int> parent;
 stack<int> s;
 
 void search(){
     int now = s.top();
     s.pop();
-    visited[now] = 1;
     for(int v: graph[now]){
-        if(!visited[v]){
+        if(!parent[v]){
             parent[v] = now;
-            visited[v] = 1;
             s.push(v);
         }
     }
@@ -29,7 +26,6 @@ int main(void){
     int n,u,v;
     cin >> n;
     parent.resize(n+1,0);
-    visited.resize(n+1,0);
     graph.resize(n+1);
     for(int i=0;i<n-1;i++){
         cin >> u >> v;
@@ -37,6 +33,7 @@ int main(void){
         graph[v].push_back(u);
     }
     s.push(1);
+    parent[1] = 1;
     do{
         search();
     }while(!s.empty());
