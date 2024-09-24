189ce783a76db58a82bfc9461a006a8ae8cf2194
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2024-09-24 21:46:49 +0900

    [Silver I] Title: 안전 영역, Time: 20 ms, Memory: 2280 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.cc
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -1,20 +1,19 @@
 #include <iostream>
-#include <vector>
 using namespace std;
     
-vector<vector<int>> arr;
-int n;
+int arr[100][100], n, h;
 int dx[4] = {1,-1,0,0};
 int dy[4] = {0,0,1,-1};
 
-void search(int x, int y, int h, vector<vector<int>> &visited){
+void search(int x, int y, int visited[100][100]){
+    int x1,y1;
     for(int i=0;i<4;i++){
-        int x1 = x+dx[i];
-        int y1 = y+dy[i];
+        x1 = x+dx[i];
+        y1 = y+dy[i];
         if(x1 < 0 || y1 < 0 || x1 >= n || y1 >= n) continue;
-        if(arr[x1][y1]>=h && !visited[x1][y1]){
+        if(arr[x1][y1]>0 && !visited[x1][y1]){
             visited[x1][y1] = 1;
-            search(x1,y1,h,visited);
+            search(x1,y1,visited);
         }
     }
 }
@@ -23,45 +22,32 @@ int main(void){
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int height[101]={0,};
-    
     cin >> n;
-    arr.resize(n,vector<int>(n));
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             cin >> arr[i][j];
         }
     }
-    int x=0,y=0,max=1;
+    int max=1;
     while(true){
-        vector<vector<int>> visited(n,vector<int>(n));
+        int visited[100][100]={0,};
         int sum=0,flag=0;
-
-        while(x<n){
-            while(y<n){
-                if(!height[arr[x][y]]){
-                    flag = 1;
-                    break;
-                }
-                y++;
-            }
-            if(flag) break;
-            y = 0;
-            x++;
-        }
-        if(!flag) break;
-        int h = arr[x][y];
-        height[h] = 1;
 
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(arr[i][j]>=h && !visited[i][j]){
+                arr[i][j]--;
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(arr[i][j]>0 && !visited[i][j]){
                     visited[i][j] = 1;
-                    search(i,j,h,visited);
+                    search(i,j,visited);
                     sum++;
                 }
             }
         }
+        if(!sum)break;
         if(max < sum) max = sum;
     }
     cout << max;
