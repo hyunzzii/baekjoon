be645ed0747a938aab74ddf7779e5c60c67e3684
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2024-10-08 16:18:54 +0900

    [Silver I] Title: 그림, Time: 16 ms, Memory: 3084 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.cc
+++ 백준/Silver/1926. 그림/그림.cc
@@ -1,21 +1,32 @@
 #include <iostream>
+#include <queue>
 #include <vector>
+#include <utility>
 using namespace std;
 
 vector<vector<int>> drawing;
+queue<pair<int,int>> q;
 int n, m;
 int dx[4] = {1,-1,0,0};
 int dy[4] = {0,0,1,-1};
 
-int DFS(int x, int y){
-    int x1,y1,sum=1;
-    for(int i=0;i<4;i++){
-        x1 = x + dx[i];
-        y1 = y + dy[i];
-        if(x1>=0 && y1>=0 && x1<n && y1<m){
-            if(drawing[x1][y1]){
-                drawing[x1][y1]=0;
-                sum += DFS(x1,y1);
+int BFS(int x, int y){
+    q.push(make_pair(x,y));
+    int x1,y1,sum = 1;
+
+    while(!q.empty()){
+        x = q.front().first;
+        y = q.front().second;
+        q.pop();
+        for(int i=0;i<4;i++){
+            x1 = x + dx[i];
+            y1 = y + dy[i];
+            if(x1>=0 && y1>=0 && x1<n && y1<m){
+                if(drawing[x1][y1]){
+                    drawing[x1][y1]=0;
+                    sum++;
+                    q.push(make_pair(x1,y1));
+                }
             }
         }
     }
@@ -40,7 +51,7 @@ int main(void){
             if(drawing[i][j]==1){
                 drawing[i][j] = 0;
                 num++;
-                tem = DFS(i,j);
+                tem = BFS(i,j);
                 max = max < tem ? tem : max;
             }
         }
