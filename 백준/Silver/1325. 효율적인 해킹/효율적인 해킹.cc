8c83e39359a678101e0136bcf0a3465fa54e4fee
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2024-11-19 04:19:47 +0900

    [Silver I] Title: 효율적인 해킹, Time: 2732 ms, Memory: 3508 KB -BaekjoonHub

---
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int n,m;
+vector<vector<int>> graph;
+
+int BFS(int k){
+    vector<bool> visited(n+1,false);
+    queue<int> q;
+
+    q.push(k);
+    int sum = 1;
+    visited[k] = true;
+    while(!q.empty()){
+        int cur = q.front();
+        q.pop();
+        for(int num: graph[cur]){
+            if(!visited[num]){
+                visited[num] = true;
+                q.push(num);
+            }
+        }
+        sum++;
+    }
+    return sum;
+}
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m;
+    graph.resize(n+1);
+    vector<int> arr(n+1,0);
+
+    int a,b;
+    while(m--){
+        cin >> a >> b;
+        graph[b].push_back(a);
+    }
+    int max = 0;
+    for(int i=1;i<=n;i++){
+        if(!graph[i].empty()){
+            int sum = BFS(i);
+            arr[i] = sum;
+            if(max <= sum){
+                max = sum;
+            }
+        }
+    }
+    for(int i=1;i<=n;i++){
+        if(arr[i] == max){
+            cout << i << " ";
+        }
+    }
+} 
