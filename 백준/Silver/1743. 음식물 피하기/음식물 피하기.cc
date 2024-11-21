a67d94ce707ff7e424c044801a2e189f386cc9fe
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2024-11-21 23:27:57 +0900

    [Silver I] Title: 음식물 피하기, Time: 0 ms, Memory: 2092 KB -BaekjoonHub

---
+++ 백준/Silver/1743. 음식물 피하기/음식물 피하기.cc
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int n,m,k;
+int arr[102][102] = {0,};
+int dx[4] = {1,-1,0,0};
+int dy[4] = {0,0,1,-1};
+
+int DFS(int row, int col, int cnt){
+    arr[row][col] = 0;
+    for(int i=0;i<4;i++){
+        int n_row = row + dx[i];
+        int n_col = col + dy[i];
+        if(arr[n_row][n_col]){
+            cnt = DFS(n_row,n_col,cnt+1);
+        }
+    }
+    return cnt;
+}
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> n >> m >> k;
+
+    int r,c;
+    while(k--){
+        cin >> r >> c;
+        arr[r][c] = 1;
+    }
+
+    int max= 0;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(arr[i][j]){
+                int tem = DFS(i,j,1);
+                if(max<tem){
+                    max = tem;
+                }
+            }
+        }
+    }
+    cout << max;
+} 
