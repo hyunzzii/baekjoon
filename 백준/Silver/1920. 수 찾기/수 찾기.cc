fe783d35f5ffd04bd63fa9626d7011f02316eeed
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2025-01-07 10:56:59 +0900

    [Silver IV] Title: 수 찾기, Time: 48 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cc
+++ 백준/Silver/1920. 수 찾기/수 찾기.cc
@@ -1,27 +1,42 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
-int n;
-vector<int> arr;
 
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
+    int n,v,m;
     cin >> n;
-    int tem,m;
+    
+    vector<int> arr(n,0);
     for(int i=0;i<n;i++){
-        cin >> tem;
-        arr.push_back(tem);
+        cin >> arr[i];
     }
     sort(arr.begin(),arr.end()); //오름차순
 
     cin >> m;
     while(m--){
-        cin >> tem;
-        cout << binary_search(arr.begin(),arr.end(),tem)<<"\n";
+        cin >> v;
+        int s=0, e=n-1, flag=0;
+        while(s<=e){
+            int m = (s+e)/2;
+            if(arr[m] == v){
+                cout << "1\n";
+                flag=1;
+                break;
+            }
+            if(arr[m] > v){
+                e = m - 1;
+            }else{
+                s = m + 1;
+            }
+        }
+        if(!flag){
+            cout <<"0\n";
+        }
     }
 } 
