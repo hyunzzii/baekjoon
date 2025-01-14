08493b48ebf77e72ef39e2fb7cd6724d838f4044
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2025-01-14 11:29:47 +0900

    [Silver I] Title: 기타 레슨, Time: 8 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.cc
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.cc
@@ -1,40 +1,35 @@
 #include <iostream>
 using namespace std;
 
-int n,m;
-int arr[100000] = {0,};
-
-int explore(int mid){
-    int sum=0,num=1;
-    for(int i=0;i<n;i++){
-        if(sum+arr[i]<=mid){
-            sum+=arr[i];
-        }
-        else if(arr[i] > mid){
-            return m+1;
-        }
-        else{
-            num++;
-            sum=arr[i];
-        }
-    }
-    return num;
-}
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
-    int sum=0;
+    
+    int n,m,s=0,e=0,mid,result;
+    int arr[100000] = {0,};
+    
     cin >> n >> m;
     for(int i=0;i<n;i++){
         cin >> arr[i];
-        sum+= arr[i];
+        e+= arr[i];
+        if(s < arr[i]){
+            s = arr[i];
+        }
     }
-    int s=0, e=sum, mid, result=sum;
+    result = e;
     while(s<=e){
         mid = (s+e)/2;
-        int num = explore(mid);
+        int sum=0,num=1;
+        for(int i=0;i<n;i++){
+            if(sum+arr[i]<=mid){
+                sum+=arr[i];
+            }
+            else{
+                num++;
+                sum=arr[i];
+            }
+        }
         if(num<=m){
             e = mid-1;
             if(mid < result){
