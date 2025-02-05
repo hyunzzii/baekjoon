7b6646453fbc5a6bcfa1aac921577d22ca436be4
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2025-02-05 10:15:01 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -1,25 +1,19 @@
-#include <iostream>
-#include <vector>
+#include <cstdio>
 using namespace std;
 
 int main(void){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    unsigned k,n;
-    cin >> k >> n;
-    vector<unsigned> arr(n,0);
-    unsigned s=1,e=0;
+    long long k,n,arr[10000]={0,};
+    scanf("%d %d",&k,&n);
+    long long s=1,e=0;
     for(int i=0;i<k;i++){
-        cin >> arr[i];
+        scanf("%d",&arr[i]);
         if(e < arr[i]){
             e= arr[i];
         }
     }
     while(s <= e){
         int sum=0;
-        unsigned mid = (s+e)/2;
+        long long mid = (s+e)/2;
         for(int i=0;i<k;i++){
             sum += (arr[i]/mid);
         }
@@ -29,5 +23,5 @@ int main(void){
             s = mid+1;
         }
     }
-    cout << e;
+    printf("%d",e);
 } 
