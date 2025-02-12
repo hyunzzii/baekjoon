5d87c532af40606ff74133c8a60e27f2aeae2be4
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2025-02-12 22:08:51 +0900

    [Gold I] Title: K번째 수, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.cc
+++ 백준/Gold/1300. K번째 수/K번째 수.cc
@@ -1,38 +1,26 @@
 #include <iostream>
 using namespace std;
 
-long long n,k;
-long long search(long long num){
-    long long s=1, e=n;
-    while(s<=e){
-        long mid = (s+e)/2;
-        if(n*mid <= num){
-            s = mid+1;
-        }else{
-            e = mid-1;
-        }
-    }
-    return e;
+long long min(int a, int b){
+    return a <= b ? a : b;
 }
-
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    
+
+    long long n,k,s,e,sum,mid;
     cin >> n >> k;
-    long long s=1,e=k;
+    s=1, e=k;
     while(s <= e){
-        long long sum=0,num;
-        long long mid = (s+e)/2;
-        num = search(mid);
-        sum+=num;
-        for(int i=n-1;i>0;i--){
-            while(i*num<=mid && num <=n){
-                num++;
-            }
-            sum += --num;
+        sum=0;
+        mid = (s+e)/2;
+        int i;
+        for(i=1;i*i<=mid;i++){
+            sum += min(mid/i,n);
         }
+        i--;
+        sum = 2*sum - i*i;
         if(sum<k){
             s = mid+1;
         }else{
