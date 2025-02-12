b47d6116e758846432a693c2f22cf0a464752488
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2025-02-12 21:44:57 +0900

    [Gold I] Title: K번째 수, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1300. K번째 수/K번째 수.cc
+++ 백준/Gold/1300. K번째 수/K번째 수.cc
@@ -23,7 +23,7 @@ int main(void){
     
     cin >> n >> k;
 
-    long long s=1,e=n*n;
+    long long s=1,e=k;
     while(s <= e){
         long long sum=0,num;
         long long mid = (s+e)/2;
@@ -33,8 +33,7 @@ int main(void){
             while(i*num<=mid && num <=n){
                 num++;
             }
-            num--;
-            sum += num;
+            sum += --num;
         }
         if(sum<k){
             s = mid+1;
