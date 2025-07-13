2767c8a976eafc046ec46e175aaef21892a025cd
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2025-07-13 17:12:48 +0900

    [Silver III] Title: 소수 구하기, Time: 20 ms, Memory: 2876 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -6,17 +6,17 @@ int main(void){
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int max =1000000;
+    int m,n;
+    cin >> m >> n;
+
     bool notPrime[1000001]={false};
     notPrime[1] = true;
     
-    for(int i=2;i<=max;i++){
-        for(int j=2;i*j<=max;j++){
+    for(int i=2;i<=n;i++){
+        for(int j=2;i*j<=n;j++){
             notPrime[i*j] = true;
         }
     }
-    int m,n;
-    cin >> m >> n;
     for(int i=m;i<=n;i++){
         if(notPrime[i]==false){
             cout << i << "\n";
