2380cd7663c66349e9383779f8b34c59d902a43d
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2025-07-14 15:35:40 +0900

    [Silver I] Title: 카잉 달력, Time: 88 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void swap(int* a, int* b){
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int T, M, N, x, y, tmp;
+    cin >> T;
+    while(T--){
+        cin >> M >> N >> x >> y;
+        if(x > y){
+            swap(&x,&y);
+            swap(&M,&N);
+        }
+        int ans=1, a=1, b=1;
+        do{
+            if(a==x && b==y){
+                break;
+            }else if(b-a == y-x){
+                a++;b++;
+                ans++;
+            }else{
+                b = (b+M)%N;
+                ans += M;
+            }
+        }while(!(a==1 && b==1) && ans <= M*N);
+        if(a==x&&b==y){
+            cout << ans << "\n";
+        }else{
+            cout << -1 <<"\n";
+        }
+    }
+}
