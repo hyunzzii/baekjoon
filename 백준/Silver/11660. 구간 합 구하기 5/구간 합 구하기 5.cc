3dd96f6630ea2a9be101688edd66d9ec65c00677
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2025-07-17 17:28:32 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 104 ms, Memory: 6132 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -1,34 +1,23 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
 
+int N, M;
+int arr[1026][1026] = {0,};
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
-    int N,M;
-    int tem;
-    cin >> N >> M;
     
-    int** arr = new int*[N+1];
-    for(int i=0; i<N+1; i++){
-        arr[i] = new int[N];
-        memset(arr[i],0,sizeof(int)*N);
-    }
-    for(int i=1; i<N+1; i++){
-        for(int j=1; j<N+1; j++){
-            cin >> tem;
-            arr[i][j] = tem + arr[i-1][j] + arr[i][j-1] - arr[i-1][j-1];
+    cin >> N >> M;
+    for(int i=1;i<=N;i++){
+        for (int j=1;j<=N;j++){
+            cin >> arr[i][j];
+            arr[i][j] += arr[i][j-1] + arr[i-1][j] - arr[i-1][j-1];
         }
     }
-    int x1,y1,x2,y2;
+    int x1,x2,y1,y2;
     while(M--){
-        cin >> x1 >> y1;
-        cin >> x2 >> y2;
+        cin >> x1 >> y1 >> x2 >> y2;
         cout << arr[x2][y2] - arr[x1-1][y2] - arr[x2][y1-1] + arr[x1-1][y1-1] << "\n";
     }
-
-    for(int i=0; i<N; i++) delete[] arr[i];
-    delete[] arr;
 }
