f0bd8a711c09bba9fc81d67a8b7cfd3da000b289
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2025-07-18 10:37:10 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -2,22 +2,22 @@
 #include <climits>
 using namespace std;
 
-int N,K;
-int arr[100001] = {0,};
-
 int main(void){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
+    int N,K,max=INT_MIN;
+    int arr[100001] = {0,};
+
     cin >> N >> K;
-    int max=INT_MIN;
     for(int i=1;i<=N;i++){
         cin >> arr[i];
         arr[i] += arr[i-1];
-        if(i >= K && max < arr[i] - arr[i-K]){
-            max = arr[i] - arr[i-K];
-        }
+    }
+    for(int i=K;i<=N;i++){
+        int tmp = arr[i] - arr[i-K];
+        if(max < tmp) max = tmp;
     }
     cout << max;
 }
