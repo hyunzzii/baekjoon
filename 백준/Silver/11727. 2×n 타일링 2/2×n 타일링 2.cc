7a0a2b9a5c1ab95fb3a41d1a5779809390677fa5
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2025-07-21 00:52:33 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 1152 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -1,19 +1,13 @@
-#include <iostream>
-using namespace std;
-
+#include <cstdio>
 int n;
 int arr[10001] = {0,};
 
 int main(void){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    cin >> n;
+    scanf("%d",&n);
     arr[1] = 1;
     arr[2] = 3;
     for(int i=3;i<=n;i++){
         arr[i] = (arr[i-1] + arr[i-2]*2) % 10007;
     }
-    cout << arr[n];
+    printf("%d",arr[n]);
 }
