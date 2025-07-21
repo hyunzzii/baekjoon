a2c2d985eba8993777f0a25c5a3a286b7e45e58e
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2025-07-21 09:14:55 +0900

    [Silver I] Title: 카드 구매하기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -1,6 +1,4 @@
-#include <iostream>
-using namespace std;
-
+#include <cstdio>
 int N;
 int arr[1001] = {0,};
 
@@ -9,16 +7,12 @@ int max(int a, int b){
 }
 
 int main(void){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    cin >> N;
+    scanf("%d",&N);
     for(int i=1;i<=N;i++){
-        cin >> arr[i];
+        scanf("%d",&arr[i]);
         for(int j=1;j<=i/2;j++){
             arr[i] = max(arr[i], arr[j] + arr[i-j]);
         }
     }
-    cout << arr[N];
+    printf("%d",arr[N]);
 }
