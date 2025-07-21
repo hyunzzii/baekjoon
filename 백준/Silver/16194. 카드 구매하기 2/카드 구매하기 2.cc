a0549b481d839bba0edb8b521afad9cfd1b9ee45
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2025-07-21 09:22:59 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.cc
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.cc
@@ -1,23 +1,17 @@
-#include <iostream>
-using namespace std;
+#include <cstdio>
 int min(int a, int b){
     return a < b ? a : b;
 }
 
 int main(void){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
     int N;
     int arr[1001] = {0,};
-
-    cin >> N;
+    scanf("%d",&N);
     for(int i=1;i<=N;i++){
-        cin >> arr[i];
+        scanf("%d",&arr[i]);
         for(int j=1;j<=i/2;j++){
             arr[i] = min(arr[i], arr[j] + arr[i-j]);
         }
     }
-    cout << arr[N];
+    printf("%d",arr[N]);
 }
