f2ea73801ac251c8a44309832401cfe2d9688468
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2025-07-21 14:47:48 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.cc
@@ -1,16 +1,11 @@
-#include <iostream>
-using namespace std;
+#include <cstdio>
 typedef long long ll;
 #define mod 1000000000
 
 int main(void){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
     int N;
     int arr[101][12] = {0,};
-    cin >> N;
+    scanf("%d",&N);
 
     for(int j=2;j<=10;j++){
         arr[1][j] = 1;
@@ -25,5 +20,5 @@ int main(void){
     for(int j=1;j<=10;j++){
         sum = (sum + arr[N][j]) % mod;
     }
-    cout << sum;
+    printf("%d",sum);
 }
