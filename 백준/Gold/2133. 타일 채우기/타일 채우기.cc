dd9a530e53509915b8b166e58a96519e44328851
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2025-07-24 03:47:31 +0900

    [Gold IV] Title: 타일 채우기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.cc
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.cc
@@ -1,13 +1,7 @@
-#include <iostream>
-using namespace std;
-
+#include <cstdio>
 int main(void){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
     int n, dp[31]={0,};
-    cin >> n;
+    scanf("%d",&n);
     dp[2] = 3;
     for(int i=4;i<=n;i+=2){
         for(int j=2;j<i;j+=2){
@@ -15,5 +9,5 @@ int main(void){
         }
         dp[i] += dp[i-2] + 2;
     }
-    cout << dp[n];
+    printf("%d",dp[n]);
 }
