5b0ec45796b8deea1cc178f71de8b030fd0253f6
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2025-07-26 00:51:26 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

---
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+int N, arr[1001], dp[1001][2] = {0,}, l[1001] = {0,}, result[1001];
+
+void change(int s, int e, int i){
+    while(s < e){
+        int mid = (s+e)/2;
+        if(dp[mid][0] < arr[i]) s = mid + 1;
+        else e = mid;
+    }
+    if(arr[i] == dp[e][0]) l[i] = -1;
+    else{
+        l[i] = dp[e-1][1];
+        dp[e][0] = arr[i];
+        dp[e][1] = i;
+    }
+}
+
+int dfs(int n){
+    if(l[n] < 0) return 0;
+    if(l[n] == 0) return 1;
+    else return dfs(l[n]) + 1;
+}
+
+void print(int dept, int i){
+    result[dept] = arr[i];
+    if(l[i] == 0) return;
+    print(dept-1, l[i]);
+}
+
+int main(void){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    for(int i=1;i<=N;i++){
+        cin >> arr[i];
+    }
+    int len = 1;
+    dp[1][0] = arr[1];
+    dp[1][1] = 1;
+    l[1] = 0;
+    for(int i=2;i<=N;i++){
+        if(dp[len][0] < arr[i]){
+            dp[++len][0] = arr[i];
+            dp[len][1] = i;
+            l[i] = dp[len-1][1];
+        }
+        else change(0, len, i);
+    }
+    cout << len << "\n";
+    for(int i=N;i>0;i--){
+        if(dfs(i) == len){
+            print(len, i);
+            break;
+        }
+    }
+    for(int i=1;i<=len;i++){
+        cout << result[i] << " ";
+    }
+}
