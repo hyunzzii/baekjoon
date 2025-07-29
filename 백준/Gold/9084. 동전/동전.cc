8e62017b52302a160d088f7d08238ab28206c229
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2025-07-29 17:39:07 +0900

    [Gold V] Title: 동전, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.cc
+++ 백준/Gold/9084. 동전/동전.cc
@@ -3,20 +3,17 @@ int main(void){
     int T, N, M, arr[20];
     scanf("%d",&T);
     while(T--){
-        int dp[10001][20] = {0,}, sum = 0;
+        int dp[10001] = {0,};
         scanf("%d",&N);
         for(int i=0;i<N;i++) scanf("%d",&arr[i]);
         scanf("%d",&M);
-        for(int i=1;i<=M;i++){
-            for(int j=0;j<N;j++){
-                for(int k=0;k<=j;k++){
-                    if(i-arr[j] < 0) break;
-                    if(i-arr[j] == 0) dp[i][j] = 1;
-                    dp[i][j] += dp[i-arr[j]][k];
-                }
+        dp[0]=1;
+        for(int i=0;i<N;i++){
+            for(int j=1;j<=M;j++){
+                if(j-arr[i] < 0) continue;
+                dp[j] += dp[j-arr[i]];
             }
         }
-        for(int i=0;i<N;i++) sum += dp[M][i];
-        printf("%d\n",sum);
+        printf("%d\n",dp[M]);
     }
 }
