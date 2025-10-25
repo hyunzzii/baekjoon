91df1a02ccca31969d65e6e4eee8ab7a9fa25f81
hyunzzii <121764785+hyunzzii@users.noreply.github.com>
2025-10-25 17:06:33 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 103.83 ms, Memory: 90.9 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.cpp
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.cpp
@@ -4,45 +4,34 @@
 using namespace std;
 
 vector<int> solution(vector<vector<int>> edges) {
-    vector<int> answer(4,0);
-    vector<vector<int>> graph(1000001);
-    vector<vector<int>> inout(1000001, vector<int>(2,0)); //(in, out)
     int num = 0;
     for(vector<int> e : edges){
-        graph[e[0]].push_back(e[1]);
-        inout[e[0]][1]++;
-        inout[e[1]][0]++;
         if(e[0] > num)num = e[0];
         if(e[1] > num)num = e[1];
     }
-    int productNum;
+    vector<int> in(num+1,0), out(num+1,0);
+    for(vector<int> e : edges){
+        in[e[1]]++;
+        out[e[0]]++;
+    }
+    int productNum, circle, bar=0, eight=0;
     for(int i=1;i<=num;i++){
-        if(inout[i][0] == 0 && inout[i][1] >= 2){
+        if(!in[i] && out[i] >= 2){
             productNum = i;
             break;
         }
     }
-    vector<bool> visited(num,false);
-    for(int n : graph[productNum]){
-        inout[n][0]--;
-        int cur = n;
-        answer[1]++;
-        while(!visited[cur]){
-            if(inout[cur][0] == 2 && inout[cur][1] == 2){
-                answer[1]--;
-                answer[3]++;
-                break;
-            }else if(!inout[cur][1]){
-                answer[1]--;
-                answer[2]++;
-                break;
-            }
-            visited[cur] = true;
-            for(int i : graph[cur]){
-                if(!visited[i]) cur = i;
-            }
+    circle = out[productNum];
+    for(int i=1;i<=num;i++){
+        if(i==productNum)continue;
+        if(out[i] == 2){
+            circle--;
+            eight++;
+        }
+        else if(in[i] && out[i] == 0){
+            circle--;
+            bar++;
         }
     }
-    answer[0] = productNum;
-    return answer;
+    return {productNum, circle, bar, eight};
 }
